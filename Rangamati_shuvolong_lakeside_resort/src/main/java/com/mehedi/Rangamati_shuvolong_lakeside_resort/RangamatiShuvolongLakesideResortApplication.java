c12adfd1e34f70813ba10e410a6df50522e678d0
Mehedi Hasan <mh.ju1803@gmail.com>
2024-03-28 22:30:49 +0600

    Project initialized

---
+++ Rangamati_shuvolong_lakeside_resort/src/main/java/com/mehedi/Rangamati_shuvolong_lakeside_resort/RangamatiShuvolongLakesideResortApplication.java
@@ -0,0 +1,13 @@
+package com.mehedi.Rangamati_shuvolong_lakeside_resort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangamatiShuvolongLakesideResortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangamatiShuvolongLakesideResortApplication.class, args);
+	}
+
+}
---
+++ Rangamati_shuvolong_lakeside_resort/src/test/java/com/mehedi/Rangamati_shuvolong_lakeside_resort/RangamatiShuvolongLakesideResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehedi.Rangamati_shuvolong_lakeside_resort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangamatiShuvolongLakesideResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
